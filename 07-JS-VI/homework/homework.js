2ed6c5de23531d64b9acb5db49bb8b1ab0765816
ferfracassi <fernando.fracassi@gmail.com>
2021-09-15 23:56:47 -0300

    Homework, con las soluciones Henry agregadas como comentarios.

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,6 +4,7 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  //return nombre[0].toUpperCase() + nombre.slice(1); <-- Solucion by Henry!!
   nombre1 = (nombre.charAt(0)).toUpperCase();
   return (nombre.replace(nombre.charAt(0), nombre1)); 
 }
@@ -11,32 +12,44 @@ function mayuscula(nombre) {
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  //cb(); <-- Solucion by Henry!!
+  //}
   usuario = cb();
- 
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-cb (n1+n2);
+  //return cb (n1+n2); <-- Solucion by Henry!!
+cb (n1+n2); 
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
-  //Tu código:
-  var suma = 0
+  /*var sumatoria = numeros.reduce(function (sum, n) { <-- Solucion by Henry!!
+    return sum + n; 
+  })
+    cb(sumatoria);
+  }               <-- Solucion by Henry!!
+  //Tu código: */
+var suma = 0
   for (i=0; i<numeros.length; i++)
   suma = (suma + numeros[i]);
   cb (suma)
 }
 
+
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  /*array.forEach(function (valor) { <-- Solucion by Henry!!
+    cb(valor);
+  })
+}                                   <-- Solucion by Henry!!*/
   for (i=0; i<array.length; i++) {
     cb (array[i]);
   }
@@ -60,7 +73,12 @@ function map(array, cb) {
 });
 return newArray;
 }
-
+/*var newArray = array.map(function(ele) { <-- Solucion by Henry!!
+  return cb(ele);
+});
+return newArray;
+}) <-- Solucion by Henry!! 
+*/
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
@@ -73,6 +91,11 @@ function filter(array) {
     })
   return(array1)
 }
+
+/*  return array.filter(function(elemento) {    <-- Solucion by Henry!!
+    return elemento[0] === "a";
+  })
+}   <-- Solucion by Henry!! */
 
 // No modificar nada debajo de esta línea
 // --------------------------------
